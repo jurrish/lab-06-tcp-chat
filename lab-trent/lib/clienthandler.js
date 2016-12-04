80ceb3b295e1248596c50294ef893a78ec59711c
Trenton Kress <trenton.kress@gmail.com>
2016-12-04 10:00:08 -0800

    Finished public messaging.

--- lab-trent/lib/clienthandler.js
+++ lab-trent/lib/clienthandler.js
@@ -11,17 +11,37 @@ function Client(conn) {
   this.socket = conn;
   conn.on('data', this.decode);
   conn.on('close', this.finish);
-  conn.on('error', handleError)
+  conn.on('error', handleError);
   clients.push(this);
 }
 
 Client.prototype.decode = function(data) {
-
+  let message = data.toString();
+  if (message.startsWith('/')) {
+    let args = message.split(' ');
+    handleCommand(args[0], args.slice(1, args.length));
+  } else {
+    sendPublic(this.nick + ': ' + message);
+  }
 };
 
 Client.prototype.finish = function() {
-
+  sendPublic(this.nick + ' disconnected.');
+  clients.slice(clients.indexOf(this));
 };
+
+function sendPublic(message) {
+  clients.forEach(function(client) {
+    client.socket.write(message);
+  });
+}
+
+function handleCommand(command, args) {
+  switch(command) {
+  case '':
+    break;
+  }
+}
 
 function handleError(err) {
   console.log(err);
