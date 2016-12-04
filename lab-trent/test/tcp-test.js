6fded40ef25517a7b66f105595f57315a0d609f0
Trenton Kress <trenton.kress@gmail.com>
2016-12-03 22:32:16 -0800

    Scaffolded project.

---
+++ lab-trent/test/tcp-test.js
@@ -0,0 +1,11 @@
+'use strict';
+
+const assert = require('assert');
+
+describe('Client', function() {
+  describe('#setNick()', function() {
+    it('should set the nickname of the Client to the specified name.', function() {
+      
+    });
+  });
+});
