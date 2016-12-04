f253a3c08f6e78bd322592de9e2b5de908f4e845
Michael Bishop <polishthebishop@gmail.com>
2016-12-04 13:01:13 -0800

    something

--- lab-michael/server.js
+++ lab-michael/server.js
@@ -1,22 +1,64 @@
 'use strict';
 
 const net = require('net');
+const EE = require('events');
+
+const Client = require('./model/client.js');
 
 const PORT = process.env.PORT || 3000; // Setup ENV vars
 
 const pool = [];
 const server = net.createServer();
+const ee = new EE();
 
-server.on('connection', function(socket) {
-  pool.push(socket);
+ee.on('connection', function(konnect) {
+  console.log('server connected',konnect);
+});
 
-  socket.on('data', function(data) {
-    pool.forEach(client => {
-      client.write(data);
-    });
+ee.on('\\nick', function(client, string) {
+  client.nickname = string.trim();
+});
+
+ee.on('\\dm', function(client, string) {
+  let nickname = string.split(' ').shift().trim();
+  let message = string.split(' ').slice(1).join(' ').trim();
+
+  pool.forEach( c => {
+    if(c.nickname === nickname) {
+      c.socket.write(`${client.nickname}: ${message}`);
+    }
   });
 });
 
+ee.on('\\all', function(client, string) {
+  pool.forEach( c => {
+    c.socket.write(`${client.nickname}: ` + string);
+  });
+});
+
+ee.on('default', function(client, string) {
+  client.socket.write('Not a valid command!' + string);
+});
+
+server.on('connection', function(socket) {
+  var client = new Client(socket);
+  pool.push(client);
+  console.log(client);
+  console.log(socket);
+
+  socket.on('data', function(data) {
+    const command = data.toString().split(' ').shift().trim();
+
+    if (command.startsWith('\\')) {
+      ee.emit(command, client, data.toString().split(' ').slice(1).join(' '));
+      return;
+    }
+
+    ee.emit('default', client, data.toString());
+
+  });
+});
 server.listen(PORT, function() {
   console.log('Server Running on PORT', PORT);
+
 });
