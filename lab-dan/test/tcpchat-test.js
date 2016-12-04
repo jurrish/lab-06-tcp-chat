9e6eb50ac33a9fbe619c68320ccffe8cb90eb996
Dan Peters <daniel.r.peters@icloud.com>
2016-12-04 09:49:08 -0800

    initial set-up of files and tests

---
+++ lab-dan/test/tcpchat-test.js
@@ -0,0 +1,19 @@
+'use strict';
+
+const tcpchat = require('../lib/tcpchat');
+const expect = require('chai').expect;
+
+describe('this is my tcpchat module', function() {
+  describe('this function starts the server', function () {
+    // start server
+  });
+  describe('this is my function that receives a message', function () {
+    // receive message
+  });
+  describe('this is my function that sends a message', function () {
+    // send message - presumably this is a separate function from the receive??
+  });
+  describe('this is my function that changes nickname', function () {
+    // bonus point if I can test this
+  });
+});
