ce627a702f36fd566d094bd2429daf78391010f1
Jessica Vasquez-Soltero <jmichellevs@gmail.com>
2016-12-04 11:03:05 -0800

    Error handling not working

--- lab-jessica/lib/server.js
+++ lab-jessica/lib/server.js
@@ -9,28 +9,23 @@ let pool = [];
 
 const server = net.createServer();
 
-// Clients should have a unique 'nickname'
-// e.g. guest-43
-// When sockets are connected with the ClientPool they should be given event listeners for data, error, and close events
+
 // When a socket emits the error event the error should be logged on the server
-// When a socket emits the data event the data should be logged on the server and the \wack commands below should be implemented
-// Wack commands '\'
-//
-// \all should trigger a broadcast event
-// \nick should allow a user change their nickname
-// \dm should allow a user to send a message directly to another user by nick name
+
 // when a user speaks their nickname should be printed
 // i.e. teapot: Sup Hacker?
 
-//////////////
-
 ee.on('\\nick', function(client, string){
   client.nickname = string.trim();
+  console.log(client.nickname);
 });
 
 ee.on('\\dm', function(client, string){
+  console.log('string', string);
   let nickname = string.split(' ').shift().trim();
   let message = string.split(' ').slice(1).join(' ').trim();
+  console.log('nickname', nickname);
+  console.log('message', message);
 
   pool.forEach( c => {
     if (c.nickname === nickname){
@@ -54,8 +49,9 @@ server.on('connection', function(socket) {
   console.log('WE ARE CONNECTED YAY');
   const client = new Client(socket);
   pool.push(client);
-  // console.log(pool); //im in the ppo;
+
   socket.on('data', function(data) {
+    console.log(data);
     const command = data.toString().split(' ').shift().trim();
 
     if(command.startsWith('\\')) {
@@ -75,6 +71,14 @@ server.on('connection', function(socket) {
     socket.destroy();
     console.log(socket.destroyed);
   });
+
+  socket.on('error', function(err) {
+    console.log('what error');
+    console.log(err);
+  });
+
+  socket.emit('error', err);
+
 });
 
 server.listen(PORT, function(){
