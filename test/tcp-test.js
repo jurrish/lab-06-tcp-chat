d0dc65065cb932bd55c9f3e7c15af986ff38e559
Rubiksron <rubiksron@gmail.com>
2016-12-04 12:58:52 -0800

    This is a tcp server for chatroom communication, with options to direct message, nickname, and close accounts.

---
+++ test/tcp-test.js
@@ -0,0 +1,14 @@
+'use strict';
+
+const assert   = require('assert');
+const mocha = require('mocha');
+const server = require('../server.js');
+
+desribe('\\nick', function(){
+  describe('nickname changing', function() {
+    it('should change nickname', function(done) {
+      
+      done();
+    });
+  });
+});
