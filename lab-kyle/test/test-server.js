ed93f4488e8cf60bddb4f0e11639214deee48503
Kyle Winckler <kyle.winckler@gmail.com>
2016-12-07 13:40:19 -0800

    working on tests

--- lab-kyle/test/test-server.js
+++ lab-kyle/test/test-server.js
@@ -2,26 +2,31 @@
 
 const assert = require('assert');
 const server = require('../server.js');
+const net = require('net');
+const client = new net.Socket();
 
 describe('a server module', function() {
   describe('data()', function() {
-    it('should add a client to the client pool', function(done) {
-
+    beforeEach(function(done) {
       server.listen(3000);
-      let pool= [];
-
-      let client = net.connect({port: 3000}, function() {
-        // do something?
+      client.connect({port: 3000}, function() {
+        done();
       });
-
-      // client.on('connection', function(socket) {
-      //   let client = new Client(socket);
-      //   pool.push(client);
-      //   assert.equal(pool.length > 0, true);
-      // });
-
+    });
+    afterEach(function(done) {
+      client.end();
       server.close();
       done();
+    });
+    describe('\\nick', function() {
+      it('should change a users nickname', function(done) {
+        client.on('data', function(data) {
+          console.log(data);
+          assert.equal(data.toString(), client.socket.nickname);
+          done();
+        });
+        client.write('\\nick Kyle');
+      });
     });
   });
 });
