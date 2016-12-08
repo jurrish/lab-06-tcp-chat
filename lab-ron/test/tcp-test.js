fb15687449c6aeeb8af72f7c06ecd016da1138ce
Rubiksron <rubiksron@gmail.com>
2016-12-07 17:35:17 -0800

    I created a lab-ron directory inside of which I moved my code, as per the directions.

--- lab-ron/test/tcp-test.js
+++ lab-ron/test/tcp-test.js
@@ -4,10 +4,10 @@ const assert   = require('assert');
 const mocha = require('mocha');
 const server = require('../server.js');
 
-desribe('\\nick', function(){
+describe('\\nick', function(){
   describe('nickname changing', function() {
     it('should change nickname', function(done) {
-      
+
       done();
     });
   });
